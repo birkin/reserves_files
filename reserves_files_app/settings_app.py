e8af162e5c2b39e1b9aa618c61741e3d3ceb1d8d
Birkin James Diana <birkin.diana@gmail.com>
2022-08-10 17:58:45 -0400

    enables dummy shib-data for local development.

--- reserves_files_app/settings_app.py
+++ reserves_files_app/settings_app.py
@@ -1,4 +1,8 @@
-import os
+import json, logging, os
+
+log = logging.getLogger(__name__)
 
 
 FILES_DIR_PATH = os.environ['RES_FILES__FILE_DIR_PATH']
+
+LOCALDEV_TEST_META_INFO: dict = json.loads( os.environ['RES_FILES__TEST_META_DCT_JSON'] )
