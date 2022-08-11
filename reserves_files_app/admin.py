cdcd1dae35a6280922b00f679463dac337759185
Birkin James Diana <birkin.diana@gmail.com>
2022-08-11 07:20:51 -0400

    adds admin filter.

--- reserves_files_app/admin.py
+++ reserves_files_app/admin.py
@@ -4,16 +4,19 @@ from reserves_files_app.models import Match
 
 class MatchAdmin( admin.ModelAdmin ):
 
-    list_display = ['id', 'filename', 'course_code', 'path', 'created']
+    list_display = ['id', 'filename', 'course_code', 'created']
+
+    list_filter = [ 'course_code' ]
 
     readonly_fields = [ 'created', 'id' ]
 
-    search_fields = [ 'id', 'filename', 'course_code', 'path', 'created' ]
+    search_fields = [ 'id', 'filename', 'course_code', 'created' ]
 
     date_hierarchy = 'created'
 
-    save_on_top = True
+    save_on_top = False
 
+    exclude = [ 'path' ]
 
 
 admin.site.register( Match, MatchAdmin )
