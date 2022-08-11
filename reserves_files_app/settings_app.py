d33ea7bed57809ecab755680c27ae8090db95e17
Birkin James Diana <birkin.diana@gmail.com>
2022-08-10 23:46:55 -0400

    adds setting from envar.

--- reserves_files_app/settings_app.py
+++ reserves_files_app/settings_app.py
@@ -6,3 +6,5 @@ log = logging.getLogger(__name__)
 FILES_DIR_PATH = os.environ['RES_FILES__FILE_DIR_PATH']
 
 LOCALDEV_TEST_META_INFO: dict = json.loads( os.environ['RES_FILES__TEST_META_DCT_JSON'] )
+
+STAFF_GROUP: str = os.environ['RES_FILES__STAFF_GROUPER_GROUP']
