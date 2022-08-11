e37cdbb4a4a804bf5f361c08c3590958fa9716c9
Birkin James Diana <birkin.diana@gmail.com>
2022-08-11 16:20:32 -0400

    adds setting.

--- reserves_files_app/settings_app.py
+++ reserves_files_app/settings_app.py
@@ -8,3 +8,5 @@ FILES_DIR_PATH = os.environ['RES_FILES__FILE_DIR_PATH']
 LOCALDEV_TEST_META_INFO: dict = json.loads( os.environ['RES_FILES__TEST_META_DCT_JSON'] )
 
 STAFF_GROUP: str = os.environ['RES_FILES__STAFF_GROUPER_GROUP']
+
+LEGIT_ADDERS: list = json.loads( os.environ['RES_FILES__LEGIT_ADDERS'] )
